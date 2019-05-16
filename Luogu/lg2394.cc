532ed63e490eb36b18cbe0fd843ebab8cbde305d
DCTewi <dctewi@dctewi.com>
2019-05-16 23:01:43 +0800

    Create lg2394.cc

---
+++ Luogu/lg2394.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long double x;
+    scanf("%15Lf", &x);
+
+    cout << fixed << setprecision(8) << x / 23.0 << "\n";
+
+    return 0;
+}
