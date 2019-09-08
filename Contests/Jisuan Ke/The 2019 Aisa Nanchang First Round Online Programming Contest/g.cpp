f7733e0485c017ec5416f7ce96959c7573103a75
DCTewi <dctewi@dctewi.com>
2019-09-08 17:15:34 +0800

    20190908 lab

---
+++ Contests/Jisuan Ke/The 2019 Aisa Nanchang First Round Online Programming Contest/g.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int T; cin >> T;
+    while (T--)
+    {
+        int n; cin>> n;
+        n == 1? puts("18000"): puts("0");
+    }
+    return 0;
+}
